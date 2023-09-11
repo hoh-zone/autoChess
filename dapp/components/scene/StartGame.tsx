0df084ec0c4a1b9e5dc506fc2ee8fd6869b3c631
SamuelQZQ <qianzheqi@foxmail.com>
2023-09-11 11:54:40 +0800

    temp mint game

--- dapp/components/scene/StartGame.tsx
+++ dapp/components/scene/StartGame.tsx
@@ -1,16 +1,20 @@
 import { Button, Center, Input, Stack } from "@chakra-ui/react"
 import { stageAtom } from "../../store/stages";
 import { useAtom } from "jotai";
+import useMint from "../MintCoin";
 
 export const StartGame = () => {
     const [stage, setStage] = useAtom(stageAtom);
-
+    const { nftObjectId, mint } = useMint();
     return (
-        <Center className="h-full w-full">
+        <Center className="h-full w-full" style={{ background: "black"}}>
             <Stack className="items-center" gap={4}>
                 <Input placeholder="Enter your name" />
                 <Button
-                    onClick={() => setStage("shop")}
+                    onClick={async () => {
+                        await mint();
+                        setStage("shop");
+                    }}
                 >Start Game</Button>
             </Stack>
         </Center>
