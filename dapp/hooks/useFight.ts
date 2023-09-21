fce31c33c3505f6eea637d44140d3ae4949e5e4e
SEAN <seansheng@yeah.net>
2023-09-21 16:37:42 +0800

    add

--- dapp/hooks/useFight.ts
+++ dapp/hooks/useFight.ts
@@ -32,7 +32,7 @@ export const useFight = () => {
             angle: randomInRange(55, 125),
             spread: randomInRange(50, 70),
             particleCount: randomInRange(50, 100),
-            origin: { y: 0.6 },
+            origin: { y: 0.6 }
         });
     };
 
