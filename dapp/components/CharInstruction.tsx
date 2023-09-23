184aec10118e4af2e2c38a64486df8d5fc9e1770
SamuelQZQ <qianzheqi@foxmail.com>
2023-09-23 11:39:28 +0800

    fix build

--- dapp/components/CharInstruction.tsx
+++ dapp/components/CharInstruction.tsx
@@ -8,11 +8,11 @@ export const CharInstruction = () => {
             <br/>
             <p>|aoe|: attack all enemies at once.</p>
             <br/>
-            <p>|add_all_hp|: add all the charactors' max hp every match, the effect is permanent even when match ends.</p>
+            <p>|add_all_hp|: add all the charactors&apos; max hp every match, the effect is permanent even when match ends.</p>
             <br/>
-            <p>|add_all_tmp_attack|: add all the charactors' attack every match, the effect is temporary within match.</p>
+            <p>|add_all_tmp_attack|: add all the charactors&apos; attack every match, the effect is temporary within match.</p>
             <br/>
-            <p>|add_all_tmp_hp|: add all the charactors' max hp every match, the effect is temporary within match.</p>
+            <p>|add_all_tmp_hp|: add all the charactors&apos; max hp every match, the effect is temporary within match.</p>
             <br/>
             <p>|forbid_buff|: if only charactor is alive, any positive buff upon enemy is invalid.</p>
             <br/>
