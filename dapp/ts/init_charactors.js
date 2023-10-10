b755745475e8fd328a84d4ead9f8b14bfbe09a55
sean <seansheng@yeah.net>
2023-10-10 22:53:01 +0800

    new contract

--- dapp/ts/init_charactors.js
+++ dapp/ts/init_charactors.js
@@ -51,7 +51,7 @@ var sigin = function (param) {
 };
 var mint_chess = function () {
     var module_name = 'role';
-    var fn = 'init_charactors1';
+    var fn = 'init_charactors2';
     var connect = function () { return __awaiter(void 0, void 0, void 0, function () {
         var provider, signer, tx, res, result;
         return __generator(this, function (_a) {
