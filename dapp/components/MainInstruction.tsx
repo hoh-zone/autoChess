eab864953745868eb1c7fbb15e82306ce7ba317a
Zongguang Zhang <zzg19931223@outlook.com>
2023-11-04 02:38:27 +0800

    MainInstruction component refactory with modal

--- dapp/components/MainInstruction.tsx
+++ dapp/components/MainInstruction.tsx
@@ -1,25 +1,38 @@
-import { useEffect, useState } from "react";
-import useQueryFight from "./button/QueryFightResult";
-import { ethos } from "ethos-connect";
-import { Button, Tooltip } from "@chakra-ui/react";
+import { useEffect, useState } from "react"
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure } from "@chakra-ui/react"
 
 export const Instruction = () => {
-    const { status } = ethos.useWallet();
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-    return <Tooltip maxWidth="900px" label={
-        <div>
-            <p>|How to play|: Every chess, you will have 10 coins to buy your charactors, different charactors own its specific feature, try to use it to gain advantage.</p>
-            <br/>
-            <p>|Level up|: charactors can compose to level up, 3 one-star can be combined into a two-star character, 3 two-stars can be combined into a three-star character.</p>
-            <br/>
-            <p>|Normal Mode|: practice and construct your lineup to fight with different players, your enemy would be other players’ past lineup records.</p>
-            <br/>
-            <p>|Arena Mode|: you have to pay 1 sui ticket fee to enter, and you can check out to get rewards at anytime, the checkout reward depends on your fighting winning records, so try to win to get earn more sui.</p>
-            <br/>
-            <p>|Problem Feedback|: suiautochess@gmail.com</p>
-        </div>
-    }>
-    <Button>Instruction</Button>
-    </Tooltip>
+  return (
+    <>
+      <Button onClick={onOpen}>Instruction</Button>
 
+      <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside" size={"lg"}>
+        <ModalOverlay />
+        <ModalContent className="w-[50vw] !max-w-[50vw]">
+          <ModalHeader>Instruction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div>
+              <p>|How to play|: Every chess, you will have 10 coins to buy your charactors, different charactors own its specific feature, try to use it to gain advantage.</p>
+              <br />
+              <p>|Level up|: charactors can compose to level up, 3 one-star can be combined into a two-star character, 3 two-stars can be combined into a three-star character.</p>
+              <br />
+              <p>|Normal Mode|: practice and construct your lineup to fight with different players, your enemy would be other players’ past lineup records.</p>
+              <br />
+              <p>|Arena Mode|: you have to pay 1 sui ticket fee to enter, and you can check out to get rewards at anytime, the checkout reward depends on your fighting winning records, so try to win to get earn more sui.</p>
+              <br />
+              <p>|Problem Feedback|: suiautochess@gmail.com</p>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
 }
