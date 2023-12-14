4f009cf52750edd0ab9cd6adfa71cf5f8582f778
sean <seansheng@yeah.net>
2023-12-14 23:08:38 +0800

    new contract

--- dapp/.history/lib/constants_20231204220807.ts
+++ dapp/.history/lib/constants_20231204220807.ts
@@ -6,8 +6,8 @@ export const ETHOS_COIN_TYPE = `${ETHOS_EXAMPLE_CONTRACT}::ethos_example_coin::E
 
 // auto_chess_vars
 export const SENDER = "0xbe379359ac6e9d0fc0b867f147f248f1c2d9fc019a9a708adfcbe15fc3130c18"
-export const PACKAGE_ID =  "0xe10b2f7cfd2c03c1ed6f4c19f9b7ec582ca0cba34d6f83105514c9888237535b"
-export const ROLE_GLOBAL =  "0x76213acc15c9982dc54754faa43fa4241a7a9e03ad7bff3446d1e771e1f83e54"
-export const LINEUP_GLOBAL =  "0x596cdfd1ca218680fb2e0ae53b8336df60188e9cdbd50c8a2f23791b4bce0668"
-export const CHESS_GLOBAL =  "0xe2a2e1e9b44b1d53d96e3e4534caca35b397b8b7bf9239547679ed02ca98c3c3"
-export const CHALLENGE_GLOBAL = "0xedd8a7ca00651340d7b1b8a0997b735238f6cc2f494e18e96079568d6d32f6a6"
+export const PACKAGE_ID =  "0x583e14062a58faf5be773df7a490d18b644387fae9a380be190ad98ad0c2e8ad"
+export const ROLE_GLOBAL =  "0xc9f7b7d585465ae0fc8834af083a6a5332ae98dfa04e1c2759bc60e86ffe9bfb"
+export const LINEUP_GLOBAL =  "0x80fb8907ab3f48ab03689adf018523944f98c8c0433c5b4eeb254ce39dc29fa8"
+export const CHESS_GLOBAL =  "0xa4990aa0997751c5299a773fdfff9e0f2a593bf6bf23ba6fb39c4798da3f177a"
+export const CHALLENGE_GLOBAL = "0xf39dea737a10599c5dab1dc24b6c7160afeb5e12d2b33f1d55e3d11a113a2657"
