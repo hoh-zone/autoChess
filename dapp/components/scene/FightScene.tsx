75661b5dcd49b798e9a9f9f5b3d6c1f41aa0d92e
Hikari <zzg19931223@outlook.com>
2023-12-23 23:40:52 +0800

    fight scene bg change by round

--- dapp/components/scene/FightScene.tsx
+++ dapp/components/scene/FightScene.tsx
@@ -2,44 +2,11 @@ import { Button, HStack, Modal, ModalBody, ModalCloseButton, ModalContent, Modal
 import { Slot } from "../control/Slot"
 import { StatusBar } from "../control/StatusBar"
 import { useAtom } from "jotai"
-import { assetsAtom, enemyCharacter, slotCharacter, winA, fightResultModalVisibleAtom, stageAtom, fightResultEffectA } from "../../store/stages"
+import { assetsAtom, enemyCharacter, slotCharacter, winA, fightResultModalVisibleAtom, stageAtom, fightResultEffectA, loseA } from "../../store/stages"
 import { twMerge } from "tailwind-merge"
 import range from "lodash/range"
 import { FightResultText } from "../effect/FightResultText"
-
-const positionTables = {
-  bg10: {
-    0: "right-0 top-0",
-    1: "right-[15%] bottom-[15%]",
-    2: "right-[30%] top-[15%]",
-    3: "right-[40%] bottom-[25%]",
-    4: "right-[50%] top-[10%]",
-    5: "right-[65%] bottom-[20%]",
-    10: "left-0 top-0",
-    11: "left-[15%] bottom-[15%]",
-    12: "left-[30%] top-[15%]",
-    13: "left-[40%] bottom-[25%]",
-    14: "left-[50%] top-[10%]",
-    15: "left-[65%] bottom-[20%]"
-  }
-}
-
-const positionTable: { [key: string]: string } = {
-  0: "right-[27%] bottom-[14%]",
-  1: "right-[50%] bottom-[14%]",
-  2: "right-[68%] bottom-[18%]",
-  3: "right-[18%] bottom-[5%]",
-  4: "right-[35%] bottom-[5%]",
-  5: "right-[65%] bottom-[5%]",
-  10: "left-[18%] bottom-[5%]",
-  11: "left-[28%] bottom-[14%]",
-  12: "left-[40%] bottom-[5%]",
-  13: "left-[50%] bottom-[12%]",
-  14: "left-[64%] bottom-[5%]",
-  15: "left-[72%] bottom-[18%]",
-  currentCharPosition: "right-[4%] bottom-[14%]",
-  currentEnemyPosition: "left-[4%] bottom-[14%]"
-}
+import { useMemo } from "react"
 
 export const FightScene = () => {
   const [enemyChars, setEnemyChars] = useAtom(enemyCharacter)
@@ -50,15 +17,14 @@ export const FightScene = () => {
   const [fightResultModalVisible, setFightResultModalVisible] = useAtom(fightResultModalVisibleAtom)
   const [fightResult, setFightResult] = useAtom(fightResultEffectA);
   const [stage, setStage] = useAtom(stageAtom);
+  const [win, _setWin] = useAtom(winA);
+  const [lose, _setLose] = useAtom(loseA);
 
-  const [win] = useAtom(winA)
-  const get_video_bg = () => {
-    let index = win + 1
-    if (index > 8) {
-      index == 8
-    }
-    return "bg" + index + ".mp4"
-  }
+  const videoBg = useMemo(() => {
+    return "bg" + ((Math.floor((win + lose) / 3)) % 3 || 1)
+  }, [])
+  // @ts-ignore
+  const positionTable:any = positionTables[videoBg] || positionTables.bg1
 
   const onClose = () => {
     setFightResultModalVisible(false)
@@ -69,7 +35,7 @@ export const FightScene = () => {
   return (
     <div className="h-full w-full relative">
       <video style={{ objectFit: "cover" }} className="w-full h-full" autoPlay loop muted>
-        <source src={assets?.bg10} type="video/mp4" />
+        <source src={assets?.[videoBg] || assets?.bg1} type="video/mp4" />
       </video>
       <HStack className="absolute top-[10%] p-8 w-full justify-around">
         <StatusBar isOpponent={false}></StatusBar>
@@ -111,4 +77,56 @@ export const FightScene = () => {
       </Modal>
     </div>
   )
+}
+
+
+const positionTables = {
+  bg1: {
+    0: "right-[27%] bottom-[14%]",
+    1: "right-[50%] bottom-[14%]",
+    2: "right-[68%] bottom-[18%]",
+    3: "right-[18%] bottom-[5%]",
+    4: "right-[35%] bottom-[5%]",
+    5: "right-[65%] bottom-[5%]",
+    10: "left-[18%] bottom-[5%]",
+    11: "left-[28%] bottom-[14%]",
+    12: "left-[40%] bottom-[5%]",
+    13: "left-[50%] bottom-[12%]",
+    14: "left-[64%] bottom-[5%]",
+    15: "left-[72%] bottom-[18%]",
+    currentCharPosition: "right-[4%] bottom-[14%]",
+    currentEnemyPosition: "left-[4%] bottom-[14%]"
+  },
+  bg2: {
+    0: "right-[27%] bottom-[14%]",
+    1: "right-[50%] bottom-[14%]",
+    2: "right-[68%] bottom-[18%]",
+    3: "right-[18%] bottom-[5%]",
+    4: "right-[35%] bottom-[5%]",
+    5: "right-[65%] bottom-[5%]",
+    10: "left-[18%] bottom-[5%]",
+    11: "left-[28%] bottom-[14%]",
+    12: "left-[40%] bottom-[5%]",
+    13: "left-[50%] bottom-[12%]",
+    14: "left-[64%] bottom-[5%]",
+    15: "left-[72%] bottom-[18%]",
+    currentCharPosition: "right-[4%] bottom-[14%]",
+    currentEnemyPosition: "left-[4%] bottom-[14%]"
+  },
+  bg3: {
+    0: "right-[27%] bottom-[14%]",
+    1: "right-[50%] bottom-[14%]",
+    2: "right-[68%] bottom-[18%]",
+    3: "right-[18%] bottom-[5%]",
+    4: "right-[35%] bottom-[5%]",
+    5: "right-[65%] bottom-[5%]",
+    10: "left-[18%] bottom-[5%]",
+    11: "left-[28%] bottom-[14%]",
+    12: "left-[40%] bottom-[5%]",
+    13: "left-[50%] bottom-[12%]",
+    14: "left-[64%] bottom-[5%]",
+    15: "left-[72%] bottom-[18%]",
+    currentCharPosition: "right-[4%] bottom-[14%]",
+    currentEnemyPosition: "left-[4%] bottom-[14%]"
+  },
 }
