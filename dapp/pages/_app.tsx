1ff69ff2d662e6f0652ede848036a96a414d22bd
sean <seansheng@yeah.net>
2024-01-07 22:56:26 +0800

    add

--- dapp/pages/_app.tsx
+++ dapp/pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     apiKey: process.env.NEXT_PUBLIC_ETHOS_API_KEY,
     preferredWallets: ['Ethos Wallet'],
     network: NETWORK,
-    chain: Chain.SUI_TESTNET
+    chain: Chain.SUI_MAINNET
   };
   const audioRef = useRef<HTMLAudioElement>(null);
   const audioFightRef = useRef<HTMLAudioElement>(null);
