73f361b62e8cbf1379fd07ce6328c27f32dddbc6
sean <seansheng@yeah.net>
2024-01-08 23:37:11 +0800

    add

--- dapp/components/button/CheckoutChess.tsx
+++ dapp/components/button/CheckoutChess.tsx
@@ -10,38 +10,37 @@ const useCheckout = () => {
     const { wallet } = ethos.useWallet();
 
     const checkout = useCallback(async ({chess_id}: Props) => {
-        alert(1)
-        // if (!wallet) return;
-        // let method = "check_out_arena"
-        // let moveModule = "chess";
-        // console.log("id:", chess_id);
-        // try {
-        //     const transactionBlock = new TransactionBlock();
-        //     transactionBlock.moveCall({
-        //         target: `${PACKAGE_ID}::${moveModule}::${method}`,
-        //         arguments: [
-        //             transactionBlock.pure(`${CHESS_GLOBAL}`),
-        //             transactionBlock.pure(`${chess_id}`),
-        //         ]
-        //     })
-        //     const response = await wallet.signAndExecuteTransactionBlock({
-        //         transactionBlock,
-        //         options: {
-        //             showObjectChanges: true,
-        //         }
-        //     });
-        //     console.log("response:", response.objectChanges);
-        //     if (response.objectChanges) {
-        //         const createObjectChange = response.objectChanges.find(
-        //             (objectChange) => objectChange.type === "created"
-        //         );
-        //         if (!!createObjectChange && "objectId" in createObjectChange) {
-        //             console.log("objid", createObjectChange.objectId);
-        //         }
-        //     }
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        if (!wallet) return;
+        let method = "check_out_arena"
+        let moveModule = "chess";
+        console.log("id:", chess_id);
+        try {
+            const transactionBlock = new TransactionBlock();
+            transactionBlock.moveCall({
+                target: `${PACKAGE_ID}::${moveModule}::${method}`,
+                arguments: [
+                    transactionBlock.pure(`${CHESS_GLOBAL}`),
+                    transactionBlock.pure(`${chess_id}`),
+                ]
+            })
+            const response = await wallet.signAndExecuteTransactionBlock({
+                transactionBlock,
+                options: {
+                    showObjectChanges: true,
+                }
+            });
+            console.log("response:", response.objectChanges);
+            if (response.objectChanges) {
+                const createObjectChange = response.objectChanges.find(
+                    (objectChange) => objectChange.type === "created"
+                );
+                if (!!createObjectChange && "objectId" in createObjectChange) {
+                    console.log("objid", createObjectChange.objectId);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }, [wallet]);
     return {checkout};
 };
