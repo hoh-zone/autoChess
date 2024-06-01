4c822ab926258b319b7ea80aadae0116380cf8b5
Tiffsimin <131053338+Tiffsimin@users.noreply.github.com>
2024-06-01 06:37:14 +0200

    Update ZkLoginButton.tsx
    ZkLoginButton done and tested half

--- dapp/components/ZkLoginButton.tsx
+++ dapp/components/ZkLoginButton.tsx
@@ -1,45 +1,65 @@
-import { Button } from "@chakra-ui/react"
+import { Button } from "@radix-ui/themes";
 import { generateNonce, generateRandomness } from '@mysten/zklogin';
 //import { jwtToAddress, getExtendedEphemeralPublicKey } from '@mysten/zklogin';
 //import { genAddressSeed, getZkLoginSignature } from "@mysten/zklogin";
 import { SuiClient } from '@mysten/sui/client';
 import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
+import { JwtPayload, jwtDecode } from "jwt-decode";
+//import { jwtDecode } from "jwt-decode";
 
-//import { JwtPayload, jwtDecode } from "jwt-decode";
-
-
-
+//pnpm build
+//pnpm run dev
 const FULLNODE_URL = 'https://fullnode.devnet.sui.io'; // replace with the RPC URL you want to use
 const suiClient = new SuiClient({ url: FULLNODE_URL });
-const {epoch} = await suiClient.getLatestSuiSystemState();
-
-const maxEpoch = Number(epoch) + 2; // this means the ephemeral key will be active for 2 epochs from now.
 const ephemeralKeyPair = new Ed25519Keypair();
+
+//Only for google acount Oauth
+const CLIENT_ID = '15551813151-do454v0l676ic4b4stm4iuorudsie7rt.apps.googleusercontent.com';
+const REDIRECT_URI = 'http://localhost:5173/';
+
+
+const {epoch} = await suiClient.getLatestSuiSystemState();
+const maxEpoch = Number(epoch) + 10; // this means the ephemeral key will be active for 2 epochs from now.
 const randomness = generateRandomness();
 const nonce = generateNonce(ephemeralKeyPair.getPublicKey(), maxEpoch, randomness);
-//Only for google acount Oauth
-const CLIENT_ID = '218247839757-v0qa1odrjfriku6mk8vac242gdgvojag.apps.googleusercontent.com';
-const REDIRECT_URI = '<YOUR_SITE_URL>';
-
 const params = new URLSearchParams({
-	// Configure client ID and redirect URI with an OpenID provider
-	client_id: CLIENT_ID,
-	redirect_uri: REDIRECT_URI,
-	response_type: 'id_token',
-	scope: 'openid',
-	// See below for details about generation of the nonce
-	nonce: nonce,
+  // Configure client ID and redirect URI with an OpenID provider
+  client_id: CLIENT_ID,
+  redirect_uri: REDIRECT_URI,
+  response_type: 'id_token',
+  scope: 'openid',
+  // See below for details about generation of the nonce
+  nonce: nonce,
 });
 
 const loginURL = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
-
-// example, use the post
-//const proofResponse = await post('/your-internal-api/zkp/get', zkpRequestPayload);
-
 /**
  * Problem 1 being:  how to use pass the client id, redirct uri and nounce to the google service, should 
  * be calling the service through the uri?
+ * Answer: Just let user click the loginURL, after user logins in its google account, google redirect to the
+ * redirect_uri passed, the id_token is attached in the redirected uri
+ * const urlFragment = window.location.hash;
+ * const urlParams = new URLSearchParams(urlFragment.substring(1));
+ * The code above is for it.
  */
+
+  // Get the URL fragment
+  const urlFragment = window.location.hash;
+
+  // Parse the fragment to get the id_token
+  const urlParams = new URLSearchParams(urlFragment.substring(1));
+  const idToken = urlParams.get('id_token');
+  if(idToken != null ){
+    const jwtPayload = jwtDecode(idToken) as JwtPayload;;
+  
+    console.log("zkLoginUserAddress iss:", jwtPayload.iss);
+    console.log("zkLoginUserAddress sub:", jwtPayload.sub);
+    console.log("zkLoginUserAddress aud:", jwtPayload.aud);
+
+  }
+
+// example, use the post
+//const proofResponse = await post('/your-internal-api/zkp/get', zkpRequestPayload);
 
 /**
  * Problem 2 being: salt
@@ -51,12 +71,10 @@ const loginURL = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
 
 /*
 
-const jwtPayload = jwtDecode(id_token);
-const decodedJwt = jwt_decode(jwtPayload) as JwtPayload;
-const zkLoginUserAddress = jwtToAddress(jwt, userSalt);
-const extendedEphemeralPublicKey = getExtendedEphemeralPublicKey(
-  ephemeralKeyPair.getPublicKey()
-);
+  const zkLoginUserAddress = jwtToAddress(jwt, userSalt);
+  const extendedEphemeralPublicKey = getExtendedEphemeralPublicKey(
+    ephemeralKeyPair.getPublicKey()
+  );
 
 const zkProofResult = await axios.post(
   "https://prover-dev.mystenlabs.com/v1",
@@ -81,13 +99,11 @@ const partialZkLoginSignature = zkProofResult as PartialZkLoginSignature
 
 
 export const ZkLoginButton = () =>{
-
     return (
-        <>
-          <Button color="cyan" variant="soft" size="3">
-                    Miao miao zk-login
-          </Button>
-
+        <>         
+           <a href={loginURL}>
+                     <Button color="cyan" variant="soft" radius="large" size="3">Login to Google </Button>
+           </a>
         </>
   )
 }
